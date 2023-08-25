0a77f66eff0689e4524edca1cf31ecfd31ce574d
Lamboprogramming <gillespielamar@gmail.com>
2023-08-25 05:04:35 -0400

    bug fixed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -59,7 +59,7 @@ const Nav = () => {
             </li>
             <li>
               <Link
-                href="/services"
+                href="/blog"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >
                 Blog
