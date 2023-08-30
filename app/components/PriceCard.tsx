9163386130a0e44885489fe8358b0d92fd9c5fcf
Lamboprogramming <gillespielamar@gmail.com>
2023-08-30 19:02:52 -0400

    text change

--- app/components/PriceCard.tsx
+++ app/components/PriceCard.tsx
@@ -12,20 +12,20 @@ const PriceCard = () => {
     <li className="mb-10 ml-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-blue-500"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">7 day delivery </time>
-        <h3 className="text-lg font-semibold text-gray-900 ">Basic services:</h3>
-        <p className="mb-4 text-base font-normal text-gray-500 leading-7 ">Personal Blogging Platform, Event Landing and Informational Site, Online Portfolio, Local Business Directory, Resume/CV Builder, Educational Resource Hub.</p>
+        <h3 className="text-lg font-semibold text-gray-900 ">Basic offers:</h3>
+        <p className="mb-4 text-base font-normal text-gray-500 leading-7 ">Personal Blogging Platform, Event Landing Page, Online Portfolio, Local Business Directory, Resume/CV Builder, Educational Resource Hub.</p>
 
     </li>
     <li className="mb-10 ml-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-blue-500"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">14 day delivery </time>
-        <h3 className="text-lg font-semibold text-gray-900 ">Standard services:</h3>
+        <h3 className="text-lg font-semibold text-gray-900 ">Standard offers:</h3>
         <p className="text-base font-normal text-gray-500 leading-7 ">Small Business Website, Online Product Showcase, Fitness and Wellness Blog, Real Estate Listings, Community Forum, Event Ticketing Platform.</p>
     </li>
     <li className="ml-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-blue-500"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">21 day delivery </time>
-        <h3 className="text-lg font-semibold text-gray-900 ">Premium services:</h3>
+        <h3 className="text-lg font-semibold text-gray-900 ">Premium offers:</h3>
         <p className="text-base font-normal text-gray-500 leading-7 ">Online Learning Management System (LMS), E-commerce Marketplace, Healthcare Appointment Booking, Travel Booking Portal, Custom Tailoring Platform, Financial Portfolio Tracker.</p>
     </li>
 </ol>
