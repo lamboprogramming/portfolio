3138840095e20032d9510a8fa07a720160d37055
Lamboprogramming <gillespielamar@gmail.com>
2023-09-12 15:11:04 -0400

    another style change

--- app/components/Jumbotron.tsx
+++ app/components/Jumbotron.tsx
@@ -6,7 +6,7 @@ const Jumbotron = () => {
       
 <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
     <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56 ">
-        <h1 className=" text-4xl font-extrabold tracking-wide text-white md:text-5xl lg:text-6xl  mb-4">Where <span className='text-teal-300'>Functionality</span> Meets Visual  <span className='text-orange-300'>Finesse</span> </h1>
+        <h1 className=" text-4xl font-bold tracking-tight text-white md:text-5xl lg:text-6xl  mb-4">Where <span className='text-teal-300'>Functionality</span> Meets Visual  <span className='text-orange-300'>Finesse</span> </h1>
        
        
      
