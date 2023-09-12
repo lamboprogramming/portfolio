99295f3747656419164458e60f65a5546042ad5d
Lamboprogramming <gillespielamar@gmail.com>
2023-09-11 23:36:04 -0400

    final test done

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,22 +10,22 @@ const Footer = () => {
 <footer className=" rounded-lg shadow  m-4">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
+            <a href="/" className="flex items-center mb-4 sm:mb-0">
                 <Image src={logo} className="h-8 mr-3 w-44" alt=" Logo" />
                 
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                    <a href="/" className="mr-4 hover:underline md:mr-6 ">About</a>
                 </li>
                 <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    <a href="/" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
                 </li>
                 <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                    <a href="/" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline">Contact</a>
+                    <a href="/" className="hover:underline">Contact</a>
                 </li>
             </ul>
         </div>
